66926761096a77de07a945d070462945eaf9a626
Diego García Miño <dgarciamino@gmail.com>
2023-12-15 19:02:05 +0100

    Project created

---
+++ src/main/java/com/diegogmino/cosmeredatabase/CosmereDatabaseApplicationLoader.java
@@ -0,0 +1,13 @@
+package com.diegogmino.cosmeredatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmereDatabaseApplicationLoader {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmereDatabaseApplicationLoader.class, args);
+	}
+
+}
